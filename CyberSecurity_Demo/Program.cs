9fb2faadd7bb6bb9bc3161766f3957e1158d4242
Vishal Gajare <vishal.gajare@capgemini.com>
2021-01-28 15:06:22 +0530

    Updated Bad code

--- CyberSecurity_Demo/Program.cs
+++ CyberSecurity_Demo/Program.cs
@@ -8,9 +8,37 @@ namespace CyberSecurity_Demo
 {
     class Program
     {
+        public class Class1
+        {
+            public decimal Calculate(decimal amount, int type, int years)
+            {
+                decimal result = 0;
+                decimal disc = (years > 5) ? (decimal)5 / 100 : (decimal)years / 100;
+                if (type == 1)
+                {
+                    result = amount;
+                }
+                else if (type == 2)
+                {
+                    result = (amount - (0.1m * amount)) - disc * (amount - (0.1m * amount));
+                }
+                else if (type == 3)
+                {
+                    result = (0.7m * amount) - disc * (0.7m * amount);
+                }
+                else if (type == 4)
+                {
+                    result = (amount - (0.5m * amount)) - disc * (amount - (0.5m * amount));
+                }
+                return result;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Cyber Sec Test Demo1");
+            Class1 obj = new Class1();
+            decimal result = obj.Calculate(5000,2,5);
+            Console.WriteLine("Result = " + result);
             Console.ReadLine();
         }
     }
